008a9dc3fd87adf5869720d8f467331f80c211cb
Lâm Phúc Tài <lphuctai@live.com>
2017-04-10 23:19:52 +0700

    Thêm các thuật toán mã hoá đối xứng cơ bản

---
+++ DataSecurity/SymmetricKey/Ceasar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSecurity.SymmetricKey
+{
+	class Ceasar
+	{
+		public static string Encrypt(string p, int k)
+		{
+			p = p.ToUpper();
+			StringBuilder c = new StringBuilder();
+			for (int i = 0; i < p.Length; i++)
+			{
+                c.Append((char)(p[i] + ((k + 26) % 26) - 65) % 26 + 65);
+			}
+			return c.ToString();
+		}
+
+		public static string Decrypt(string c, int k)
+		{
+			c = c.ToUpper();
+			Console.Write(c + " " + k + " ");
+			Console.WriteLine(Encrypt(c, -1 * k));
+			return Encrypt(c, -1 * k);
+		}
+	}
+}
---
+++ DataSecurity/SymmetricKey/Vigenere.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSecurity.SymmetricKey
+{
+	class Vigenere
+	{
+		public static string Encrypt(string p, string k)
+		{
+			p = p.ToUpper();
+			StringBuilder c = new StringBuilder();
+			StringBuilder key = new StringBuilder();
+			while (p.Length > key.Length)
+				key.Append(k);
+			for(int i = 0; i < p.Length; i ++)
+			{
+				c.Append(Ceasar.Encrypt(p[i].ToString(), key[i] - 65));
+			}
+			return c.ToString();
+		}
+
+		public static string Decrypt(string c, string k)
+		{
+			c = c.ToUpper();
+			StringBuilder p = new StringBuilder();
+			StringBuilder key = new StringBuilder();
+			while (c.Length > key.Length)
+				key.Append(k);
+			for (int i = 0; i < c.Length; i++)
+			{
+				p.Append(Ceasar.Decrypt(c[i].ToString(), key[i] - 65));
+			}
+			return p.ToString();
+		}
+	}
+}
