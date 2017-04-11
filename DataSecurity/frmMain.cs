d4640a3ba26ca6d7d96d107de5159ddad19f230c
Lâm Phúc Tài <lphuctai@live.com>
2017-04-11 09:39:27 +0700

    Vẫn đang sửa lỗi xuất nhiều lần trên forrm

--- DataSecurity/frmMain.cs
+++ DataSecurity/frmMain.cs
@@ -17,172 +17,36 @@ namespace Ceasar_Playfair_Vigenere
         public frmDataSecurity()
         {
             InitializeComponent();
-        }
+		}
 
+		// Form load
+		private void frmDataSecurity_Load(object sender, EventArgs e)
+		{
+			cbboxType.SelectedIndex = 0;
+		}
 
-        private void btnInputPlainText_Click(object sender, EventArgs e)
-        {
-            plainTextFileDialog.Filter = "Text File| *.txt";
-            plainTextFileDialog.ShowDialog();
-        }
+		#region Link đến các thuật toán
 
-        private void btnGeneratCipher_Click(object sender, EventArgs e)
-        {
-            /* try
-            {
-                //Đọc ND từ StreamReader sang string
-                StreamReader srPlainText = new StreamReader(plainTextFileDialog.OpenFile());
-                string plain_text = srPlainText.ReadToEnd();
-                plain_text = plain_text.Trim().ToLower().Replace(" ", string.Empty);
-
-                //Mã hoá
-                StringBuilder sb = new StringBuilder();
-                foreach (char c in plain_text)
-                {
-                    char c1 = (char)((c - 97 + key)%26 + 65 );
-                    sb.Append(c1);
-                }
-                string cipher_text = sb.ToString();
-                MessageBox.Show("Plain text: " + plain_text + "\r\nKey: " + key.ToString() + "\r\nCipher text: " + cipher_text);
-
-                //Ghi vào file mã hoá
-                StreamWriter sw = File.CreateText(plainTextFileDialog.FileName.Replace(".txt", "-cipher.txt"));
-                foreach (char c in cipher_text)
-                    sw.Write(c);
-                sw.Close();
-                srPlainText.Close();
-
-                //Khoá nút Mã hoá, mở nút Mở tập tin Mã hoá
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Vui lòng nhập khoá!");
-            }
-            catch(FileNotFoundException)
-            {
-                MessageBox.Show("Vui lòng nhập bản rõ!");
-            }   */
-        }
-
-        private void btnReceivePlain_Click(object sender, EventArgs e)
-        {
-            dialogGetPlainText.Filter = "Text File| *.txt";
-            dialogGetPlainText.ShowDialog();
-            txtPlainInputURL.Text = dialogGetPlainText.FileName;
-        }
-
-        private void btnKeyInput_Click(object sender, EventArgs e)
-        {
-            dialogGetKey.Filter = "Text File| *.txt";
-            dialogGetKey.ShowDialog();
-            txtGetKey.Text = dialogGetKey.FileName;
-        }
-        string Ceasar_Encrypt(string plain, string key)
-        {
+		// Mã hoá Ceasar
+		string Ceasar_Encrypt(string plain, string key)
+		{
 			int k = 0;
 			try
 			{
 				k = Convert.ToInt32(key);
 			}
-			catch(Exception)
+			catch (Exception)
 			{
 				MessageBox.Show("Bạn chỉ có thể nhập vào một số nguyên cho loại mã hoá này.", "Lưu Ý"
 					, MessageBoxButtons.OK, MessageBoxIcon.Warning);
 				return "";
 			}
 
-            return Ceasar.Encrypt(plain, k);
-        }
-        string Playfair_Encrypt(string plain, string key)
-        {
-            return Playfair.Encrypt(plain, key);
-        }
-        string Vigenere_Encrypt(string plain, string key)
-        {
-            return Vigenere.Encrypt(plain, key);
-        }
-        string TinyA5_Encrypt(string plain, string key)
-        {
-            return plain;
-        }
-        string A5_Encrypt(string plain, string key)
-        {
-            return plain;
-        }
-        string TinyDES_Encrypt(string plain, string key)
-        {
-            return plain;
-        }
-        string DES_Encrypt(string plain, string key)
-        {
-            return plain;
-        }
-        private void btnEncrypt_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                //Chép bản rõ vào biến string
-                StreamReader srPlainText = new StreamReader(dialogGetPlainText.OpenFile());
-                string plain_text = srPlainText.ReadToEnd();
-                plain_text = plain_text.Trim().ToLower().Replace(" ", string.Empty);
+			return Ceasar.Encrypt(plain, k);
+		}
 
-                //Chép khoá vào biến string
-                StreamReader srKey = new StreamReader(dialogGetKey.OpenFile());
-                string key = srKey.ReadToEnd();
-                key = key.Trim().ToLower().Replace(" ", string.Empty);
-
-                //Thực hiện mã hoá theo lựa chọn
-                string cipher = "";
-                switch (cbboxType.SelectedItem.ToString())
-                {
-                    case "Ceasar":      cipher = Ceasar_Encrypt(plain_text, key); break;
-                    case "Playfair":    cipher = Playfair_Encrypt(plain_text, key); break;
-                    case "Vigenere":    cipher = Vigenere_Encrypt(plain_text, key); break;
-                    case "Tiny A5/1":   cipher = TinyA5_Encrypt(plain_text, key); break;
-                    case "A5/1":        cipher = A5_Encrypt(plain_text, key); break;
-                    case "Tiny DES":    cipher = TinyDES_Encrypt(plain_text, key); break;
-                    case "DES":         cipher = DES_Encrypt(plain_text, key); break;
-                }
-
-                dialogSaveEncryption.Filter = "Text file | *.txt";
-                dialogSaveEncryption.ShowDialog();
-                txtOpenCipherText.Text = dialogSaveEncryption.FileName;
-
-                StreamWriter sw = new StreamWriter(dialogSaveEncryption.OpenFile());
-                sw.WriteLine(cipher);
-                sw.Close();
-                MessageBox.Show("Mã hoá thành công!");
-
-                dialogSaveEncryption.Reset();
-            }
-            catch (IndexOutOfRangeException)
-            {
-                MessageBox.Show("Thiếu tập tin Bản rõ/Khoá/Bản mã! Yêu cầu nhập đầy đủ!");
-            }
-        }
-        private void frmDataSecurity_Load(object sender, EventArgs e)
-        {
-            cbboxType.SelectedIndex = 0;
-        }
-        private void btnOpenCipherText_Click(object sender, EventArgs e)
-        {
-            System.Diagnostics.Process.Start(txtOpenCipherText.Text);
-        }
-
-        private void btnGetCipher_Click(object sender, EventArgs e)
-        {
-            dialogGetCipherText.Filter = "Text File| *.txt";
-            dialogGetCipherText.ShowDialog();
-            txtGetCipherURL.Text = dialogGetCipherText.FileName;
-        }
-        private void btnGetKey2_Click(object sender, EventArgs e)
-        {
-            dialogGetKey2.Filter = "Text File| *.txt";
-            dialogGetKey2.ShowDialog();
-            txtGetKey2.Text = dialogGetKey2.FileName;
-        }
-        string Ceasar_Decrypt(string cipher_text, string key)
-        {
+		string Ceasar_Decrypt(string cipher_text, string key)
+		{
 			int k = 0;
 			try
 			{
@@ -197,31 +61,167 @@ namespace Ceasar_Playfair_Vigenere
 
 			return Ceasar.Decrypt(cipher_text, k);
 		}
-        string Playfair_Decrypt(string cipher_text, string key)
+
+		// Mã hoá Playfair
+		string Playfair_Encrypt(string plain, string key)
+		{
+			return Playfair.Encrypt(plain, key);
+		}
+
+		string Playfair_Decrypt(string cipher_text, string key)
+		{
+			return Playfair.Decrypt(cipher_text, key);
+		}
+
+		// Mã hoá Vigenere
+		string Vigenere_Encrypt(string plain, string key)
+		{
+			return Vigenere.Encrypt(plain, key);
+		}
+
+		string Vigenere_Decrypt(string cipher_text, string key)
+		{
+			return Vigenere.Decrypt(cipher_text, key);
+		}
+
+		// Mã hoá TinyA5
+		string TinyA5_Encrypt(string plain, string key)
+		{
+			return plain;
+		}
+
+		string TinyA5_Decrypt(string cipher_text, string key)
+		{
+			return cipher_text;
+		}
+
+		// Mã hoá A5
+		string A5_Encrypt(string plain, string key)
+		{
+			return plain;
+		}
+
+		string A5_Decrypt(string cipher_text, string key)
+		{
+			return cipher_text;
+		}
+
+		// Mã hoá TinyDES
+		string TinyDES_Encrypt(string plain, string key)
+		{
+			return plain;
+		}
+
+		string TinyDES_Decrypt(string cipher_text, string key)
+		{
+			return cipher_text;
+		}
+
+		// Mã hoá DES
+		string DES_Encrypt(string plain, string key)
+		{
+			return plain;
+		}
+
+		string DES_Decrypt(string cipher_text, string key)
+		{
+			return cipher_text;
+		}
+
+		#endregion
+
+		// Lấy file chứa bản rõ (mã hoá)
+		private void btnReceivePlain_Click(object sender, EventArgs e)
+		{
+			dialogGetPlainText.Filter = "Text File| *.txt";
+			dialogGetPlainText.ShowDialog();
+			txtPlainInputURL.Text = dialogGetPlainText.FileName;
+		}
+
+		// Lấy file khoá (mã hoá)
+		private void btnKeyInput_Click(object sender, EventArgs e)
+		{
+			dialogGetKey.Filter = "Text File| *.txt";
+			dialogGetKey.ShowDialog();
+			txtGetKey.Text = dialogGetKey.FileName;
+		}
+
+		// Chọn file xuất bản mã (mã hoá)
+		private void btnOpenCipherText_Click(object sender, EventArgs e)
         {
-            return Playfair.Decrypt(cipher_text, key);
+            System.Diagnostics.Process.Start(txtOpenCipherText.Text);
         }
-        string Vigenere_Decrypt(string cipher_text, string key)
+
+
+		// Lấy file chứa bản mã (giải mã)
+        private void btnGetCipher_Click(object sender, EventArgs e)
         {
-            return Vigenere.Decrypt(cipher_text, key);
-        }
-        string TinyA5_Decrypt(string cipher_text, string key)
+            dialogGetCipherText.Filter = "Text File| *.txt";
+            dialogGetCipherText.ShowDialog();
+            txtGetCipherURL.Text = dialogGetCipherText.FileName;
+		}
+		
+		// Lấy file khoá (giải mã)
+		private void btnGetKey2_Click(object sender, EventArgs e)
+		{
+			dialogGetKey2.Filter = "Text File| *.txt";
+			dialogGetKey2.ShowDialog();
+			txtGetKey2.Text = dialogGetKey2.FileName;
+		}
+
+		// Chọn file xuất bản rõ (giải mã)
+		private void btnOpenPlainText_Click(object sender, EventArgs e)
         {
-            return cipher_text;
+            System.Diagnostics.Process.Start(txtOpenPlainText.Text);
         }
-        string A5_Decrypt(string cipher_text, string key)
-        {
-            return cipher_text;
-        }
-        string TinyDES_Decrypt(string cipher_text, string key)
-        {
-            return cipher_text;
-        }
-        string DES_Decrypt(string cipher_text, string key)
-        {
-            return cipher_text;
-        }
-        private void btnDecrypt_Click(object sender, EventArgs e)
+
+		// Chức năng mã hoá
+		private void btnEncrypt_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				//Chép bản rõ vào biến string
+				StreamReader srPlainText = new StreamReader(dialogGetPlainText.OpenFile());
+				string plain_text = srPlainText.ReadToEnd();
+				plain_text = plain_text.Trim().ToLower().Replace(" ", string.Empty);
+
+				//Chép khoá vào biến string
+				StreamReader srKey = new StreamReader(dialogGetKey.OpenFile());
+				string key = srKey.ReadToEnd();
+				key = key.Trim().ToLower().Replace(" ", string.Empty);
+
+				//Thực hiện mã hoá theo lựa chọn
+				string cipher = "";
+				switch (cbboxType.SelectedItem.ToString())
+				{
+					case "Ceasar": cipher = Ceasar_Encrypt(plain_text, key); break;
+					case "Playfair": cipher = Playfair_Encrypt(plain_text, key); break;
+					case "Vigenere": cipher = Vigenere_Encrypt(plain_text, key); break;
+					case "Tiny A5/1": cipher = TinyA5_Encrypt(plain_text, key); break;
+					case "A5/1": cipher = A5_Encrypt(plain_text, key); break;
+					case "Tiny DES": cipher = TinyDES_Encrypt(plain_text, key); break;
+					case "DES": cipher = DES_Encrypt(plain_text, key); break;
+				}
+
+				dialogSaveEncryption.Filter = "Text file | *.txt";
+				dialogSaveEncryption.ShowDialog();
+				txtOpenCipherText.Text = dialogSaveEncryption.FileName;
+
+				StreamWriter sw = new StreamWriter(dialogSaveEncryption.OpenFile());
+				sw.WriteLine(cipher);
+				sw.Close();
+				MessageBox.Show("Mã hoá thành công!");
+
+				dialogSaveEncryption.Reset();
+			}
+			catch (IndexOutOfRangeException)
+			{
+				MessageBox.Show("Thiếu tập tin Bản rõ/Khoá/Bản mã! Yêu cầu nhập đầy đủ!");
+			}
+		}
+
+		// Chức năng giải mã
+		private void btnDecrypt_Click(object sender, EventArgs e)
         {
             try
             {
@@ -263,10 +263,6 @@ namespace Ceasar_Playfair_Vigenere
             {
                 MessageBox.Show("Thiếu tập tin Bản rõ/Khoá/Bản mã! Yêu cầu nhập đầy đủ!");
             }
-        }
-        private void btnOpenPlainText_Click(object sender, EventArgs e)
-        {
-            System.Diagnostics.Process.Start(txtOpenPlainText.Text);
         }
 
     }
